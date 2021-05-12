edc4ae980b114707d373491e777bb79c0498ca57
Lorena Lopes <loren4lopes@gmail.com>
2021-05-12 12:40:47 -0300

    Correction of entities

--- src/main/java/com/lorena/springcourse/repository/RequestRepository.java
+++ src/main/java/com/lorena/springcourse/repository/RequestRepository.java
@@ -1,12 +1,24 @@
 package com.lorena.springcourse.repository;
 
 import java.util.List;
+
+
 import com.lorena.springcourse.domain.Request;
+import com.lorena.springcourse.domain.enums.RequestState;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long>{ //Nome da classe que o repository pertence e o tipo de dados da chave primária
     
     public List<Request> findAllByOwnerId(Long id);
+
+    @Transactional(readOnly = false)
+    @Modifying //Informa que essa consulta irá modificar a entidade
+    @Query("UPDATE request SET state = ?2 WHERE id = ?1")
+    public int updateStatus(Long id, RequestState state);
 }
--- src/main/java/com/lorena/springcourse/repository/RequestStageRepository.java
+++ src/main/java/com/lorena/springcourse/repository/RequestStageRepository.java
@@ -2,17 +2,14 @@ package com.lorena.springcourse.repository;
 
 import java.util.List;
 
-import com.lorena.springcourse.domain.Request;
 import com.lorena.springcourse.domain.RequestStage;
-import com.lorena.springcourse.domain.enums.RequestState;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestStageRepository extends JpaRepository<RequestStage, Long> {
     
-    public List<RequestStage> findAllByRequest(Long id);
+    public List<RequestStage> findAllByRequestId(Long id);
 
-    @Query("UPDATE request SET status = ?2 WHERE id = ?1")
-    public Request updateStatus(Long id, RequestState state);
 }
