2d86961a27d7f3d63e27d93600437da6396aa0a4
Lorena Lopes <loren4lopes@gmail.com>
2021-05-13 15:24:56 -0300

    Create RequestStageResource

---
+++ src/main/java/com/lorena/springcourse/resource/RequestStageResource.java
@@ -0,0 +1,33 @@
+package com.lorena.springcourse.resource;
+
+import com.lorena.springcourse.domain.RequestStage;
+import com.lorena.springcourse.service.RequestStageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "request-stages")
+public class RequestStageResource {
+    @Autowired private RequestStageService requestStageService;
+
+    @PostMapping
+    public ResponseEntity<RequestStage> save(@RequestBody RequestStage requestStage){
+        RequestStage createdRequestStage = requestStageService.save(requestStage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdRequestStage);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<RequestStage> getById(@PathVariable(name = "id") Long id){
+        RequestStage requestStage = requestStageService.getById(id);
+        return ResponseEntity.ok(requestStage);
+    }
+}
