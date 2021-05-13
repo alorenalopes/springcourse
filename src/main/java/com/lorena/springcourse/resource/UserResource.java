7ef4df12a98ac0f71e354215ebca4cdbb968d127
Lorena Lopes <loren4lopes@gmail.com>
2021-05-13 15:25:38 -0300

    Correction in RequestMapping

--- src/main/java/com/lorena/springcourse/resource/UserResource.java
+++ src/main/java/com/lorena/springcourse/resource/UserResource.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //Controlador Rest
-@RequestMapping(name = "users")
+@RequestMapping(value = "users")
 public class UserResource {
     @Autowired private UserService userService;
     @Autowired private RequestService requestService;
