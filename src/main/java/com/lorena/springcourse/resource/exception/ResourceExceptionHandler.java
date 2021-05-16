c561d86996b8f09f1841376c9167a152d3f28139
Lorena Lopes <loren4lopes@gmail.com>
2021-05-16 16:05:25 -0300

    Remove methods unnecessary

--- src/main/java/com/lorena/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/lorena/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -1,21 +1,17 @@
 package com.lorena.springcourse.resource.exception;
 
-import java.util.ArrayList;
+
 import java.util.Date;
-import java.util.List;
 
 import com.lorena.springcourse.exception.NotFoundException;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
@@ -28,44 +24,16 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
-	@ExceptionHandler(BadCredentialsException.class)
-	public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex) {
-		ApiError error = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
-		
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-	}
-	
-	@ExceptionHandler(AccessDeniedException.class)
-	public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException ex) {
-		ApiError error = new ApiError(HttpStatus.FORBIDDEN.value(), ex.getMessage(), new Date());
-		
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
-	}
-	
-	@ExceptionHandler(MaxUploadSizeExceededException.class)
-	public ResponseEntity<ApiError> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
-		String[] parts = ex.getMessage().split(":");
-		String msg = parts[parts.length - 1].trim().toUpperCase();
-		
-		ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), msg, new Date());
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-	}
-	
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		List<String> errors = new ArrayList<String>();
-		ex.getBindingResult().getAllErrors().forEach(error -> {
-			errors.add(error.getDefaultMessage());
-		});
 		
-		String defaultMessage = "Invalid field(s)";
+			String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+			ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
 		
-		ApiErrorList error = new ApiErrorList(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date(), errors);
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
 }
