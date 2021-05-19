0332b7491f0941a8980cb0ea1ee52896ee144e0e
Lorena Lopes <loren4lopes@gmail.com>
2021-05-19 13:54:18 -0300

    Implement UserDetailsService

--- src/main/java/com/lorena/springcourse/service/UserService.java
+++ src/main/java/com/lorena/springcourse/service/UserService.java
@@ -1,5 +1,6 @@
 package com.lorena.springcourse.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,10 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     
     @Autowired private UserRepository userRepository;
 
@@ -65,5 +71,20 @@ public class UserService {
     public int updateRole(User user){
 
         return userRepository.updateRole(user.getId(), user.getRole());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> result = userRepository.findByEmail(username);
+
+        if(!result.isPresent()) throw new UsernameNotFoundException("Doesn't exist user with email = " + username);
+        
+        User user = result.get();
+
+        List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())); //.name porque o Role é uma string e "ROLE_" para ser compreendido como uma permissão
+        //asList -> Lista pré definida de elementos
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getName(), authorities);
+    
     }
 }
