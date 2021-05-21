37736b265d4f66971a459f86e6b353e432b4c33c
Lorena Lopes <loren4lopes@gmail.com>
2021-05-21 15:46:56 -0300

    Ignore login endpoint

--- src/main/java/com/lorena/springcourse/security/SecurityConfig.java
+++ src/main/java/com/lorena/springcourse/security/SecurityConfig.java
@@ -5,11 +5,14 @@ import com.lorena.springcourse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+       web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
+       //Rota será liberada sem autenticação
     }
 
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
